c17fde5ee4f3821ee8501d36937e33bd2c2dde43
ttwange <86237194+ttwange@users.noreply.github.com>
2022-03-11 11:39:06 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,12 @@
-nclude <stdio.h>
-
-
-
+#include <stdio.h>
 #include <unistd.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 1 (Success)
- *
- *     */
-
+ * main - Entry point
+ * Return:1
+ */
 int main(void)
-
 {
-
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	return (1);
-
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
+return (1);
 }
