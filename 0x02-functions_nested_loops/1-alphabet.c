62e381275ba846676f211dc9bffc991a7e83876f
Thonne Twange <ttwange2@gmail.com>
2022-03-15 09:53:32 -0700

    added 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ * main- print aphabetical letters
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{	
+	int i;
+	
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+
+
+}
