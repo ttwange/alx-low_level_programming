4dca008dd65635a49bd60c49afdff255f89d4fa2
Thonne Twange <ttwange2@gmail.com>
2022-03-15 10:15:47 -0700

    added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_islower -checks whether is lower or not
+ *@c: ASCII code character
+ *
+ * Return : 0 if not and 1 if lower
+ */
+int _islower(int c)
+{
+	if(c >= 97 && c <= 122)
+	{
+		return(10);
+	}
+	else
+	{
+		return(0);
+	}
+	_putchar('\n');
+}
