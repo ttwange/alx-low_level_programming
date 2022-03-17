733bf9ba8a8e711ae21023ef6b83facdd5a66a58
Thonne Twange <ttwange2@gmail.com>
2022-03-17 07:28:25 -0700

    added

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -causes infinte loop
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinte loop incoming :(\n');
+
+			i =0;
+			/*
+			 * while (i <0)
+			 * {
+			 * Infinte loop - No increase of variable
+			 * putchar(i);
+			 }
+			 */
+			print(=f('Infinte loop avoided! \\0/\n');
+				return (0);
+				}
+
