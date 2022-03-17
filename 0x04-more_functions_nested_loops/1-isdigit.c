349842f32318cec65744dbe3fbdcadb752474008
Thonne Twange <ttwange2@gmail.com>
2022-03-17 13:49:28 -0700

    added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit checks for digit 
+ * @c: character
+ * Return 1 for digit and 0 not digit
+ */
+int _isdigit(int c)
+{
+	for(c >= 48 && c <= 57)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
