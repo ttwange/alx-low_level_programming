a04107cb2dda5d9c5a69b6274f2f96334d8246e8
Thonne Twange <ttwange2@gmail.com>
2022-03-17 13:35:48 -0700

    added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main _isupper9(int c) checks  code for upper case
+ *@c: character input
+ * Return 1 if uppercase and 0 if lower case
+ */
+
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return(1)
+	}
+	else
+	{
+		return(0)
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_number(void);
+void print_most_numbers(void);
+void more_number(void);
+void print_line(int n);
+void print_diagonal(int i);
+void print_square(int size);
+void print_triangle(int size);
+
+
+
+
+#endif
