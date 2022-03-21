9dcdd19b0c1acbdde76747f3b5748391e227585f
Thonne Twange <ttwange2@gmail.com>
2022-03-21 09:18:24 -0700

    added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen -returns length ofa string
+ *
+ *
+ * Return - always 0
+ */
+int _string(char *s)
+{
+	char *s;
+	int len;
+
+	str = "cuea";
+	len _strlen(str);
+	*str = len;
+	putchar("%d\n",*s);
+	return(0);
+}
