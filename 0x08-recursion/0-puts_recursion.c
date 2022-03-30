3a8aaafe5b1232b926e447caae866e55e4c825cd
ttwange <86237194+ttwange@users.noreply.github.com>
2022-03-31 00:30:18 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,4 +3,18 @@
 /**
  *
  *_puts_recursion - prints a string
- *@s: pointer
+ *@s: characters
+ *Return: nothing
+ */
+
+void _puts_recursion(car *s)
+{
+  if(*s != '\0')
+  {
+    _putchar(*s);
+    _puts_recursion(s + 1);
+  }
+  else
+    
+    _putchar('\n');
+}
