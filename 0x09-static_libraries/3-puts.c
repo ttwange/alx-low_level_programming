a9a5de7e411d27342b953f2bd3d01b9e1422c86c
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-03 01:00:07 +0300

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 /**
- *_puts - prints a string, followed by a new line
- *@str: pointer to the string 
- *Return: void
- */
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+
 void _puts(char *str)
 {
-	return(0);
+int i = 0;
+while (str[i])
+{
+	_putchar(str[i]);
+	i++;
+}
+_putchar('\n');
 }
