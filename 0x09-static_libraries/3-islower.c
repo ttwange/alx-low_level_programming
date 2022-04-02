559b64f0ce49b1d1e2af9cff0044b9e3f180f2e7
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-03 00:28:38 +0300

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,20 +1,10 @@
 #include "main.h"
+ 
 
-/**
- * _islower - checks whether is lower or not
- *@c: Ascii code character
- *
- * Return: 0 if it is not, 1 if it is lower.
- */
+
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+
+	return ( c >= 'a' && c <= 'z');
+
 }
