95ab5076935f75605c7281dcec37cb65132c4db2
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-03 18:24:38 +0300

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
-/**
- *_isupper - determines whether a character is upper or lower case.
- *@c: character input
- *Return: 1 if upper, 0 if lower.
- */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
+
+
 	}
-	else
+	
+	else 
 	{
 		return (0);
 	}
