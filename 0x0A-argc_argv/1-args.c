670ecadc425b4446b133661208ccdf9604663fbb
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-03 22:07:51 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main 
- *
- *
- *
+ * main - display num of arg
+ * @argc:int
+ * @argv:string
+ * Return:int
  */
-
-int main( int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-		printf("%d\n", argc -1);
+
+		printf("%d\n", argc - 1);
+
 	return (0);
 }
-
