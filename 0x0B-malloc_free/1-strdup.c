128e23ca61b0b00ba542ff4e14f59401469e0951
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-05 23:59:50 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,2 +1,29 @@
-#include
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - points to a duplicate string
+ * @str: the string to compare
+ * Return: pointer to the duplicate or NULL
+ */
+char *_strdup(char *str)
+{
+	char *m;
+	unsigned int i, j;
 
+	if (str == NULL)
+		return (NULL);
+
+	for (j = 0; str[j] != '\0'; j++)
+		;
+
+	m = (char *)malloc(sizeof(char) * (j + 1));
+
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i <= j; i++)
+	{
+		m[i] = str[i];
+	}
+	return (m);
+}
