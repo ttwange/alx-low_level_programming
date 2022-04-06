66cddf8ab5ad8074bf3c81a0885815cad7d0d764
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-06 22:47:32 +0300

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
+/**
+ * _abs - computes absolute value of an integer
+ * @n: integer to be passed
+ *
+ * Return: function return 0
+ */
 int _abs(int n)
 {
-
-	if ( n < 0)
+	if (n > 0)
 	{
-
-		return (n * -1);
-
+		n = n * 1;
 	}
-
-	else 
+	else if (n == 0)
 	{
-
-		return (n);
-
+		n = 0;
 	}
-
-
-
+	else if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
 }
