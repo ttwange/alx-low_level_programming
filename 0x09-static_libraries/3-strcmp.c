0ce4bb84696f845a039b537a5794a43253b68a2a
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-06 22:42:51 +0300

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
-/**
- *_strcmp - compares strings
- *@s1: first string
- *@s2: second string
- *Return: 0
- */
 
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Return: an integer less than, equal to, or greater than zero
+ */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	int i, output;
 
-	while (op == 0)
-	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
+	i = 0;
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i) != '\0')
 		i++;
-	}
-	return (op);
+	output = (*(s1 + i) - *(s2 + i));
+
+	return (output);
 }
