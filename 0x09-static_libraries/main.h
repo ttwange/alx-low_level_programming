c54edb3e1c1b14119ac92ba299bba72d0cdb4e1c
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-06 22:49:40 +0300

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,11 @@
-#ifndef MAIN_FUNC
-#define MAIN_FUNC
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
 
 int _putchar(char c);
 int _islower(int c);
