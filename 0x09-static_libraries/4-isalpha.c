66f075c0761aea3834bb917d528200b5a9f7bf63
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-06 22:44:51 +0300

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
+/**
+ * _isalpha - checks for alphabetic characters.
+ *
+ * @c: char type letter
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase and 0 otherwise
+ */
 
 int _isalpha(int c)
 {
- 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else 
-		return 0;
-
-
-
-
+	return ((c >= 97 && c <= 123) || (c >= 65 && c <= 91));
 }
