0e994355097d4e269c8a7b4699b498593da87966
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-06 22:32:53 +0300

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,20 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+ * _isdigit - checks for a digit
+ * @c: input
+ * Return: 1 if c is a digit
+ * 0 otherwise
+ */
 int _isdigit(int c)
 {
-	if ((c >= 48) && (c <= 57))
+	if ((c >= '0') && (c <= '9'))
 	{
 		return (1);
-
 	}
-	
-	else 
+	else
 	{
-
 		return (0);
-
 	}
-
-
 }
