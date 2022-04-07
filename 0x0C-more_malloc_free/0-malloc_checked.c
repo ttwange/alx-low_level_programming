09f7ebb244d483cd06d794485a90ad2bf261136a
Thonne Twange <ttwange2@gmail.com>
2022-04-07 14:04:54 -0700

    added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_checked - checks for allcoated memory
+ *@b: integer pointer
+ *Return: Nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b)
+
+	if ( p == NULL)
+		exit(98);
+
+	return(p);
+}
