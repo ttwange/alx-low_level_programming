9fd78490fc953478159cea1c7200a7fc7bf87149
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-08 01:51:05 +0300

    Update 1-string_ncocat.c

--- 0x0C-more_malloc_free/1-string_ncocat.c
+++ 0x0C-more_malloc_free/1-string_ncocat.c
@@ -1,51 +1,31 @@
-char * string_nconcat(char * s1, char * s2, unsigned int n) {
+char * string_nconcat(char * s1, char * s2, unsigned int n) {
+    char * ptr;
+    unsigned int i, len1, len2;
 
-	    char * ptr;
+    if (!s1)
+      s1 = "";
+    if (!s2)
+      s2 = "";
 
-	        unsigned int i, len1, len2;
+    for (len1 = 0;*(s1 + len1); len1++)
+    ;
+    for (len2 = 0;*(s2 + len2); len2++)
+    ;
 
+    if (len2 < n)
+      n = len2;
 
+    ptr = malloc((len1 + n + 1) * sizeof(char));
 
-		    if (!s1)
+    if (!ptr)
+      return (NULL);
 
-			          s1 = "";
-
-		        if (!s2)
-
-				      s2 = "";
-
-
-
-			    for (len1 = 0;*(s1 + len1); len1++)
-
-				        ;
-
-			        for (len2 = 0;*(s2 + len2); len2++)
-
-					    ;
-
-
-
-				    if (len2 < n)
-
-					          n = len2;
-
-
-
-				        ptr = malloc((len1 + n + 1) * sizeof(char));
-
-
-
-					    if (!ptr)
-
-						          return (NULL);
-
-
-
-					        for (i = 0; i < (len1 + n); i++) {
-
-							      if (i < len1)
-
-								              ptr[i] = s1[i];
-
-							            e
+    for (i = 0; i < (len1 + n); i++) {
+      if (i < len1)
+        ptr[i] = s1[i];
+      else
+        ptr[i] = s2[i - len1];
+    }
+    ptr[i] = '\0';
+  return (ptr);
+}
