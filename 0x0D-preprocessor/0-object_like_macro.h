692aec8bbd10048fdd472020893d0a5a3a10fd15
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-11 22:58:40 +0300

    Update 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,18 +1,3 @@
 #include <stdio.h>
-#include "0-object_like_macro.h"
 #define SIZE 1024
-
-/**
- * main - check the code
- * 
- *Return: 0
- */
-
-int main()
-{
-	int s;
-	
-	s = 0 + SIZE;
-	printf("%d\n",SIZE);
-	return (0);
-}
+printf(size)
