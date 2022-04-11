1ef146c11d2f732b9d1ea26888e93db214fc5af5
Thonne Twange <ttwange2@gmail.com>
2022-04-11 16:39:45 -0700

    added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
  * new_dog - creates a new dog
  * @name: new dog name
  * @age: new dog age
  * @owner: dog owner
+ *
  * Return: NULL if it fails
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *store;
@@ -16,24 +19,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (store == NULL || name == NULL || owner == NULL)
 	{
 		free(store);
-		return (NULL);
+		return(NULL);
 	}
 	for (j = 0; name[j]; j++)
 	{
 		;
 	}
-	for (k = 0; owner[k]; k++)
+	for(k =0; owner[k]; k++)
 	{
 		;
-	}
-	store->name = malloc(j + 1);
-	store->owner = malloc(k + 1);
-	if (store->name == NULL || store->owner == NULL)
-	{
-		free(store->name);
-		free(store->owner);
-		free(store);
-		return (NULL);
 	}
 	store->name = name;
 	store->age = age;
