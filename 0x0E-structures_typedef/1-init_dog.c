34ff0b8ae1f367726b338f3167d217140c587de7
Thonne Twange <ttwange2@gmail.com>
2022-04-11 15:53:05 -0700

    added

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,15 @@
-#include "dog.h"
+#include "dog"
+
 /**
  * init_dog - initializes dog struct
  * @d: new name
  * @name: dog name
- * @age: dog age
+ * @age: dog owner
  * @owner: dog owner
+ *
  * Return: Nothing
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
