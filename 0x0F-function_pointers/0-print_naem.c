3dd346d136e1c8f462cc7d7dd4ed29a878107787
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-14 01:57:44 +0300

    Update 0-print_naem.c

--- 0x0F-function_pointers/0-print_naem.c
+++ 0x0F-function_pointers/0-print_naem.c
@@ -1,2 +1,13 @@
 #include "function_pointers.h"
 
+/**
+ *print_name: function to print name
+ *
+ *Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{ 
+  if( f != NULL)
+          f(name);
+}
