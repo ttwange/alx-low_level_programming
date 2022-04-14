ca9b014ca919e3fd2a8318c0615624bf166c5add
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-14 23:41:56 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,1 +1,52 @@
 #include "variadic_functions.h"
+
+/**
+ * print_all - prints everything
+ * @format: pointer to a character that
+ * will determine what is to be printed
+ * based on cfis.
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arglist;
+	unsigned int i = 0, j, k = 0;
+	char *str;
+	const char args[] = "cifs";
+
+	va_start(arglist, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (args[j])
+		{
+			if (format[i] == args[j] && k)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(arglist, int)), k = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(arglist, int)), k = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(arglist, double)), k = 1;
+			break;
+		case 's':
+			str = va_arg(arglist, char *), k = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
+	}
+	printf("\n"), va_end(arglist);
+}
