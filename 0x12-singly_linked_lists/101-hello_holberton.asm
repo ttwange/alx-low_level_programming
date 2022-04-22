13d162c46e3a64f736be3a61c45a392ab53ade16
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-23 00:25:23 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,16 @@
-#incliude
+; Filename: hello_holberton.asm
+; Author: Adam Keino
+
+global 	main
+extern 	printf
+
+section .text
+
+main:
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
