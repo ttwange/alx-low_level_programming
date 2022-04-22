ee8c0f7869a4c755965bb65c84940554cda067fc
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-23 00:21:12 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,17 @@
-#include
+#include "lists.h"
+/**
+ * list_len - returns number of elements in a list
+ * @h: pointer to the list
+ * Return: length of list
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
