7b320cdb23c9f36e22eb83277261ef366971405c
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-26 18:49:57 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,19 @@
-#include
+#include "lists.h"
+/**
+ * free_listint2 - frees list
+ * @head: pointer to pointer to start of list
+ * return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
