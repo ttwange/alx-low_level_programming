82563d2b38a1729cb7713e898b2412af41329e24
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-26 18:46:35 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,17 @@
-#include
+#include "lists.h"
+/**
+ * listint_len - prints number of elements
+ * @h: pointer to the list
+ * Return: Number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
