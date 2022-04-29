510f555beb52f5e4bdd22ec05d490a34ba92da42
Thonne Twange <ttwange2@gmail.com>
2022-04-29 03:25:54 -0700

    added

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,1 +1,25 @@
-#include
+#include "main.h"
+
+/**
+ * int flip_bits - flip to get one number to another
+ * @n: unsined long integer
+ * @m: unsigned long integer
+ *
+ * return: number of bits 
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int t;
+	unsigned int h = 0;
+
+
+	t = n ^m;
+
+	while(t)
+	{
+		h += t & 1;
+		t >>= 1;
+	}
+return (t);
+}
