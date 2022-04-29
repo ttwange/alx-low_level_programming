8be8b4e59a9aabd8d620d960b80edcff6c12cf46
ttwange <86237194+ttwange@users.noreply.github.com>
2022-04-29 12:51:08 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts to binary number
- * @b : pointer to a string 0 and 1
- *
- * Return: the converted number, or 0 if
- * there is one or more chars in the string b that is not 0 or 1
- * b is NULL
+ * binary_to_uint - converts a binary
+ * @b: pointer char
+ * Return: the converted number
  */
 
-int main(void)
+unsigned int binary_to_uint(const char *b)
 {
+	unsigned int a, k = 0;
 
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+		if (b[a] == '1')
+			k = k * 2 + 1;
+		else
+			k = k * 2 + 0;
+	}
+	return (k);
+}
