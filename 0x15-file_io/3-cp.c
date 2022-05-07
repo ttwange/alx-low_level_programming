8040b2681c544ab146730e319fe5f95e8ef234f4
ttwange <86237194+ttwange@users.noreply.github.com>
2022-05-07 17:37:47 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,95 +1,77 @@
 #include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 /**
- * check97 - function that checks for error exit97
- * @ac: number of arguments
- * Return: Nothing
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-void check97(int ac)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (ac != 3)
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-}
-/**
- * check98 - function that checks for error exit98
- * @filedes: file descriptor
- * @file: name of the file
- * Return: Nothing
- */
-void check98(int filedes, char *file)
-{
-	if (filedes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-}
-/**
- * check99 - function that checks for error exit99
- * @filedes: file descriptor
- * @file: name of the file
- * Return: Nothing
- */
-void check99(int filedes, char *file)
-{
-	if (filedes == -1)
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
 
 /**
- * check100 - function that checks for error exit100
- * @filedes: file descriptor
- * @fd: file destination
- * Return: Nothing
- */
-void check100(int filedes, int fd)
-{
-	if (filedes == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-/**
- * main - a program that copies the content of a file to another file
- * @argc: argument count
- * @argv: arguments from input
- * Return: (0) Success
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int fd1, fd2;
-	char *file_from, *file_to;
-	char content[1024];
-	int rd, wrt, cl1, cl2;
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
-	check97(argc);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
 
-	file_from = argv[1];
-	fd1 = open(file_from, O_RDONLY);
-	check98(fd1, file_from);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	file_to = argv[2];
-	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	check99(fd2, file_to);
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
 
-	do {
-		rd = read(fd1, content, 1024);
-		check98(fd1, file_from);
-		wrt = write(fd2, content, rd);
-		if (wrt == -1 || wrt != rd)
-			check99((-1), file_to);
-	} while (wrt == 1024);
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
-	cl1 = close(fd1);
-	check100(cl1, fd1);
-	cl2 = close(fd2);
-	check100(cl2, fd2);
-
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
 }
