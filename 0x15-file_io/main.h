d8cacecc6f51d6e63995fdee4922e9e75066d3ab
ttwange <86237194+ttwange@users.noreply.github.com>
2022-05-07 17:35:54 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,12 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-
+int _putchar(void);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+size_t _strlen(char *);
 int append_text_to_file(const char *filename, char *text_content);
-
-#endif 
+void error_file(int file_from, int file_to, char *argv[]);
+int main(int argc, char *argv[]);
+#endif
