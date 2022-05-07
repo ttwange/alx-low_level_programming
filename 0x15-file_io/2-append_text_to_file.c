9bc15dc8b090a7588f5fd2512c9779e7ebb6eb89
ttwange <86237194+ttwange@users.noreply.github.com>
2022-05-07 17:37:23 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,46 +1,45 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 /**
- * _strlen - function
- * Description: a function that returns the length of the string
- * @s: string to measure
- * Return: length of s
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
  */
-int _strlen(char *s)
+size_t _strlen(char *str)
 {
-	/*check if s string is at null byte*/
-	if (!*s)
-	{
-		return (0);
-	}
-	/*if not, return 1 and go dive into function and add one*/
-	/*this is synonymous to doing i++ when using while loop*/
-	return (1 + _strlen(s + 1));
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
 }
 
 /**
- * append_text_to_file - function that appends text at the end of a file.
- * @filename: the name of the file
- * @text_content: content string to add at the end of the file
+ * append_text_to_file - appends a text at the end of a file.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
  * Return: 1 on success and -1 on failure
- * -1 if file does not exit or if you do not have the required permission to
- * write the file
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	long int fd;
-	long int len;
-	long int wrt;
+	int fd;
+	ssize_t len;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
-		len = _strlen(text_content);
-	wrt = write(fd, text_content, len);
-	if (wrt == -1)
-		return (-1);
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
+	if (len == -1)
+		return (-1);
 	return (1);
 }
