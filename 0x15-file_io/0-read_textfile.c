4d3d67430dba15233da13e5a178102a323ccdd17
ttwange <86237194+ttwange@users.noreply.github.com>
2022-05-07 17:22:56 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,30 +1,50 @@
 #include "main.h"
 /**
-* read_textfile - read the text file
-* @filename: name of the file
-* @letters: size of text
-* Return: the actual number of the letters
-*/
+ * read_textfile - function that reads a text file and prints it to the
+ * POSIX (Portable Operating System Interface (IX)) standard output.
+ * @filename: name of file in string
+ * @letters: number of letters to read from file
+ * Return: the actual number of letters it could read and print.
+ * If the file can not open or read, returns 0
+ * If the filename is NULL, returns 0
+ * If write fails or does not write the expected amount of bytes, return 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	int opn, rd;
+	char *content;
+	int fd, rd;
+	long int wrt;
 
-	if (filename == NULL)
+	content = malloc((letters + 1) * sizeof(char *));
+	if (filename == NULL || content == NULL)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
+		close(fd);
+		free(content);
 		return (0);
 	}
-	opn = open(filename, O_RDONLY);
-	if (opn < 0)
+	rd = read(fd, content, letters);
+	if (rd == -1)
 	{
-		free(buffer);
+		close(fd);
+		free(content);
 		return (0);
 	}
-	rd = read(opn, buffer, letters);
-	write(STDOUT_FILENO, buffer, rd);
-	close(opn);
-	return (rd);
+	content[letters] = '\0';
+
+	wrt = write(STDOUT_FILENO, content, rd);
+
+	if (wrt < rd)
+	{
+		close(fd);
+		free(content);
+		return (0);
+	}
+
+	close(fd);
+	free(content);
+	return (wrt);
 }
